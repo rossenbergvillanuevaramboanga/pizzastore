4ee8f86edb452c6daf2e42247e7f36ed48d7e0e9
rossenbergvillanuevaramboanga <rossenebrgvillanueva.ramboanga@gmail.com>
2022-12-24 11:26:21 +0100

    Added missing files to repo

---
+++ src/main/java/it/prova/pizzastore/PizzastoreApplication.java
@@ -0,0 +1,13 @@
+package it.prova.pizzastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzastoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzastoreApplication.class, args);
+    }
+
+}
---
+++ src/main/java/it/prova/pizzastore/model/Pizza.java
@@ -0,0 +1,29 @@
+package it.prova.pizzastore.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@Setter @Getter
+@NoArgsConstructor @AllArgsConstructor
+
+@Entity
+@Table(name="pizza")
+public class Pizza {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "descrizione")
+    private String descrizione;
+    @Column(name = "ingredienti")
+    private String ingredienti;
+    @Column(name = "prezzoBase")
+    private Integer prezzoBase;
+
+    @Column(name = "attivo")
+    private boolean attivo;
+
+}
