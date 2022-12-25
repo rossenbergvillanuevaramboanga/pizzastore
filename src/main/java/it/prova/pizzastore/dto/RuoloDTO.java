a6f59f4617e71ceaa532859676441dbd68ab20d5
rossenbergvillanuevaramboanga <rossenebrgvillanueva.ramboanga@gmail.com>
2022-12-26 00:08:45 +0100

    Implemented RuoloDTO

--- src/main/java/it/prova/pizzastore/dto/RuoloDTO.java
+++ src/main/java/it/prova/pizzastore/dto/RuoloDTO.java
@@ -1,4 +1,36 @@
 package it.prova.pizzastore.dto;
 
+import it.prova.pizzastore.model.Ruolo;
+import lombok.*;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RuoloDTO {
+
+    private Long id;
+    private String descrizione;
+    private String codice;
+
+    public static RuoloDTO buildRuoloDTOFromModel(Ruolo ruoloModel) {
+        return new RuoloDTO(ruoloModel.getId(), ruoloModel.getDescrizione(), ruoloModel.getCodice());
+    }
+
+    public static List<RuoloDTO> createRuoloDTOListFromModelSet(Set<Ruolo> modelListInput) {
+        return modelListInput.stream().map(ruoloEntity -> {
+            return RuoloDTO.buildRuoloDTOFromModel(ruoloEntity);
+        }).collect(Collectors.toList());
+    }
+
+    public static List<RuoloDTO> createRuoloDTOListFromModelList(List<Ruolo> modelListInput) {
+        return modelListInput.stream().map(ruoloEntity -> {
+            return RuoloDTO.buildRuoloDTOFromModel(ruoloEntity);
+        }).collect(Collectors.toList());
+    }
 }
